03e0d62dda4c4523fbdfa7e9ae049104044a3979
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2016-04-13 18:13:16 +0800

    finish change template engine and add bootstrap grid

--- server.js
+++ server.js
@@ -2,32 +2,15 @@ var express = require("express");
 var ip   = "127.0.0.1";
 var app = express();
 var fs = require('fs');
-app.set('view engine', 'jade');
-
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
 function start(){
 
 
     app.get('/', function (req, res) {
-        // var elements = [ // list of graph elements to start with
-        //     { // node a
-        //       data: { id: 'a' }
-        //     },
-        //     { // node b
-        //       data: { id: 'b' }
-        //     },
-        //     { // node c
-        //       data: { id: 'c' }
-        //     },
-        //     { // edge ab
-        //       data: { id: 'ab', source: 'a', target: 'b' }
-        //     },
-        //     { // edge ba
-        //       data: { id: 'ba', source: 'b', target: 'a' }
-        //     }
-        // ];
         data = fs.readFileSync('data/all.js', 'utf8');
         elements = JSON.parse(data);
-        res.render('index', { title: 'HiDOS-Proteomics', message: 'HiDOS-Proteomics!', elements: elements});
+        res.render('index', { title: 'HiDOS-Proteomics',message: 'HiDOS-Proteomics!',elements: elements});
     });
 
     app.listen(3000, function () {
